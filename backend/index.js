11f3ac309eb178c4f78363f273ea6d41f266a2f8
sharathchandra596 <sharathjoshi596@gmail.com>
2024-11-17 19:36:05 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const app = express()
 dotenv.config()
 app.use(express.json())
 app.use(cors({
-    origin: ["http://localhost:5173"],
+    origin: [process.env.FRONTEND_URL],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"]
 }))
